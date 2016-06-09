9aec8cdcd76bda5b17aec738849606b41b32f00a
James Arthur <thruflo@gmail.com>
2016-06-09 18:07:41 +0100

    requeue: fix memory leak by polling in a new process.

--- src/ntorque/work/requeue.py
+++ src/ntorque/work/requeue.py
@@ -8,20 +8,55 @@ __all__ = [
     'RequeuePoller',
 ]
 
-from . import patch
-patch.green_threads()
-
 import logging
 logger = logging.getLogger(__name__)
 
+import Queue
+
+import multiprocessing
 import time
 
+from datetime import datetime
 from redis.exceptions import RedisError
 from sqlalchemy.exc import SQLAlchemyError
 
 from pyramid_redis.hooks import RedisFactory
 from ntorque import model
 from .main import Bootstrap
+
+def call_in_process(f, *args, **kwargs):
+    """Calls a function or method with the args and kwargs provided
+      in a new process. We use this to make sure we don't get a memory
+      leak from the db queries -- given that this thing runs forever.
+    """
+
+    # Configure.
+    timeout = kwargs.get('process_timeout', 10) # seconds
+
+    # Prepare a wrapper function and cross-process queue to get the
+    # return value from.
+    q = multiprocessing.Queue()
+    def doit():
+        r = f(*args, **kwargs)
+        q.put(r)
+
+    # Run the function in a new process and put the result on the queue.
+    p = multiprocessing.Process(target=doit)
+    p.start()
+
+    # Block until the function is called or the timeout is reached.
+    # If the latter, terminate the process.
+    p.join(timeout=timeout)
+    if p.is_alive():
+        p.terminate()
+
+    # Now, if there's a value on the queue, return it.
+    return_value = None
+    try:
+        return_value = q.get(False)
+    except Queue.Empty:
+        pass
+    return return_value
 
 class RequeuePoller(object):
     """Polls the database for tasks that should be re-queued."""
@@ -31,6 +66,7 @@ class RequeuePoller(object):
         self.channel = channel
         self.delay = delay
         self.interval = interval
+        self.call_in_process = kwargs.get('call_in_process', call_in_process)
         self.get_tasks = kwargs.get('get_tasks', model.GetDueTasks())
         self.logger = kwargs.get('logger', logger)
         self.session = kwargs.get('session', model.Session)
@@ -44,29 +80,34 @@ class RequeuePoller(object):
 
         while True:
             t1 = self.time.time()
-            try:
-                tasks = self.get_tasks()
-            except SQLAlchemyError as err:
-                self.logger.warn(err, exc_info=True)
-            else:
-                if tasks:
-                    for task in tasks:
-                        try:
-                            self.enqueue(task)
-                        except RedisError as err:
-                            self.logger.warn(err, exc_info=True)
-                        self.time.sleep(self.delay)
-            finally:
-                self.session.remove()
+            tasks = self.call_in_process(self.query)
+            if tasks:
+                for task in tasks:
+                    try:
+                        self.enqueue(*task)
+                    except RedisError as err:
+                        self.logger.warn(err, exc_info=True)
+                    self.time.sleep(self.delay)
             current_time = self.time.time()
             due_time = t1 + self.interval
             if current_time < due_time:
                 self.time.sleep(due_time - current_time)
 
-    def enqueue(self, task):
+    def query(self):
+        tasks = []
+        with transaction.manager:
+            try:
+                tasks = [(x.id, x.retry_count) for x in self.get_tasks()]
+            except SQLAlchemyError as err:
+                self.logger.warn(err, exc_info=True)
+            finally:
+                self.session.remove()
+        return tasks
+
+    def enqueue(self, id_, retry_count):
         """Push an instruction to re-try the task on the redis channel."""
 
-        instruction = '{0}:{1}'.format(task.id, task.retry_count)
+        instruction = '{0}:{1}'.format(id_, retry_count)
         self.redis.rpush(self.channel, instruction)
 
 class ConsoleScript(object):
