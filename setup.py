118504744fec3fbf60f309002b8c33c4301a49c6
James Arthur <thruflo@gmail.com>
2016-09-09 16:26:54 +0100

    release: v0.4.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'ntorque',
-    version = '0.4.0',
+    version = '0.4.1',
     description = 'Web hook task queue service.',
     author = 'James Arthur',
     author_email = 'username: thruflo, domain: gmail.com',
